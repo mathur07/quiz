837afae264ffceb773a165af51f56fe06cd2173f
Agus Fikri Suyuthi <ikrydev@gmail.com>
2020-10-16 01:16:33 +0700

    Enhance unit test for components folder

--- tests/unit/components/Header.spec.js
+++ tests/unit/components/Header.spec.js
@@ -1,28 +1,54 @@
-
 // Libraries
-import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
 // Utilities
-import { createLocalVue, mount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils';
 
 // Components
-import Header from '@/components/Header.vue'
+import Header from '@/components/Header.vue';
 
 Vue.use(Vuetify);
-
 const localVue = createLocalVue();
 
+jest.mock('@/event-bus.js', () => {
+  const EventBus = {
+    $on: jest.fn(),
+    $emit: jest.fn(),
+    $off: jest.fn(),
+  };
+
+  global.EventBus = EventBus;
+  return global.EventBus;
+});
+
 describe('Header.vue', () => {
-    let vuetify
-    beforeEach(() => {
-        vuetify = new Vuetify();
+  let vuetify, wrapper;
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(Header, {
+      localVue,
+      vuetify,
     });
-    test('snapshot', () => {
-        const wrapper = mount(Header, {
-            localVue,
-            vuetify,
-        })
-        expect(wrapper).toMatchSnapshot
-    });
+  });
+
+  it('should listen EventBus event on mounted', async () => {
+    expect(global.EventBus.$on).toBeCalledWith(
+      'update-question-count',
+      expect.any(Function)
+    );
+  });
+
+  test('should update index when updateIndex method invoked', () => {
+    expect(wrapper.vm.index).toBe(0);
+    wrapper.vm.updateIndex(1);
+
+    expect(wrapper.vm.index).toBe(1);
+  });
+
+  test('should $off EventBus when component has been destroyed', () => {
+    wrapper.destroy();
+
+    expect(global.EventBus.$off).toBeCalledWith('update-question-count');
+  });
 });
--- tests/unit/components/Question.spec.js
+++ tests/unit/components/Question.spec.js
@@ -1,28 +1,88 @@
-
 // Libraries
-import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
 // Utilities
-import { createLocalVue, mount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils';
 
 // Components
-import Question from '@/components/Question.vue'
+import Question from '@/components/Question.vue';
 
 Vue.use(Vuetify);
-
 const localVue = createLocalVue();
 
+jest.mock('@/event-bus.js', () => {
+  const EventBus = {
+    $on: jest.fn(),
+    $emit: jest.fn(),
+    $off: jest.fn(),
+  };
+
+  global.EventBus = EventBus;
+  return global.EventBus;
+});
+
+const question = {
+  category: 'Science & Nature',
+  type: 'multiple',
+  difficulty: 'easy',
+  question:
+    'Rhinoplasty is a surgical procedure on what part of the human body?',
+  correct_answer: 'Nose',
+  incorrect_answers: ['Ears', 'Chin', 'Neck'],
+};
+
+const options = ['Nose', 'Ears', 'Chin', 'Neck'];
+
 describe('Question.vue', () => {
-    let vuetify
-    beforeEach(() => {
-        vuetify = new Vuetify();
+  let vuetify, wrapper;
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(Question, {
+      localVue,
+      vuetify,
+      propsData: {
+        question,
+        options,
+      },
     });
-    test('snapshot', () => {
-        const wrapper = mount(Question, {
-            localVue,
-            vuetify,
-        })
-        expect(wrapper).toMatchSnapshot
-    });
+  });
+
+  it('should set selectedOption properly', () => {
+    const answer = 'Nose';
+    wrapper.vm.setSelectedOption(answer);
+
+    expect(wrapper.vm.selectedOption).toBe(answer);
+    expect(global.EventBus.$emit).toBeCalledWith('selected-option', answer);
+  });
+
+  it('should reset selectedOption properly', () => {
+    wrapper.vm.resetSelectedOption();
+
+    expect(wrapper.vm.selectedOption).toBe('');
+    expect(wrapper.vm.selectedOptionIndex).toBe(undefined);
+  });
+
+  it('should set selectedOption to empty string when option unselected', async () => {
+    const answer = 'Nose';
+    wrapper.vm.setSelectedOption(answer);
+
+    wrapper.vm.selectedOptionIndex = true;
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.selectedOptionIndex).toBe(true);
+    expect(wrapper.vm.selectedOption).toBe(answer);
+
+    wrapper.vm.selectedOptionIndex = false;
+
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.selectedOptionIndex).toBe(false);
+    expect(wrapper.vm.selectedOption).toBe('');
+  });
+
+  test('should $off EventBus when component has been destroyed', () => {
+    wrapper.destroy();
+
+    expect(global.EventBus.$off).toBeCalledWith('question-changed');
+  });
 });
